0384fa44855bd78d11b87dd31d060bd22f52d314
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-09 19:55:18 +0100

    this commit will add login form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <label for="CIN">CIN</label>
+            <input type="text"><br>
+            <label for="password">password</label>
+            <input type="password"><br>
+            <input type="submit" value="Log In">
+        </form>
+    </body>
+</html>
