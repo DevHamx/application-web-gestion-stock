9829e97218f0fc80ec25564955b01ab22cae4e1d
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-09 20:05:19 +0100

    add php code to check login and password

--- index.php
+++ index.php
@@ -5,12 +5,29 @@
         <title>Login</title>
     </head>
     <body>
-        <form method="POST" action="">
+        <form method="POST" action="index.php">
             <label for="CIN">CIN</label>
-            <input type="text"><br>
+            <input type="text" name="CIN"><br>
             <label for="password">password</label>
-            <input type="password"><br>
+            <input type="password" name="password"><br>
             <input type="submit" value="Log In">
         </form>
+        <?php
+            if(isset($_POST["CIN"])&&isset($_POST['password'])){
+                $var=0;
+                $login1=$_POST['CIN'];
+                $password1=$_POST['password'];
+                $con=mysqli_connect("localhost","root","");
+                    mysqli_select_db($con,'aumk');
+                    $reponse = mysqli_query($con,"select * from utilisateurs");
+                    while($donnees = mysqli_fetch_array($reponse)){
+                        if((strcmp($login1,$donnees['CIN'])==0)&&(strcmp($password1,$donnees['password'])==0)){
+                            header("location:page.html");
+                        }
+                        else $var=1;}
+                 mysqli_close($con);
+                 if($var==1)
+                  echo"error";}
+        ?>
     </body>
 </html>
