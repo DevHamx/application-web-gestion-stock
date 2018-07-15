46e78366d2b6d7bc9ffde3fa393a3fd6cca5f315
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-15 15:28:10 +0100

    add the Rechercher_categorie1 and put its link in navbar

---
+++ Rechercher_categorie1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/signin.css" rel="stylesheet">
+        <title>Rechercher une catégorie</title>
+    </head>
+    <body>
+        <?php 
+        include 'navbar.html';?>
+    </body>
+</html>
