e2fc8d4bda32b33c730aca65c4076fc5a0703c73
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-15 20:39:11 +0100

    add Rechercher_categorie1.php

--- Rechercher_categorie1.php
+++ Rechercher_categorie1.php
@@ -8,7 +8,87 @@
         <title>Rechercher une catégorie</title>
     </head>
     <body>
-        <?php 
+        <?php
         include 'navbar.html';?>
+        <main role="main" class="container">
+        <form class="form-signin" method="POST" action="Rechercher_categorie1.php">
+        <h1 style="color:#0a8ab4;" class="text-center h3 mb-3 font-weight-bold text-uppercase">Recherche dans la Catégorie 1</h1>
+        <select id="method" name="method" class="custom-select" onchange="change_method()">
+            <option disabled selected>Recherche Par :</option>
+            <option value="ID de la catégorie">ID de la catégorie</option>
+            <option value="Nom de la catégorie">Nom de la catégorie</option>
+        </select><br><br>
+        <div id="valeur" >
+        <input  class="form-control" type="text" name="valeur" required ></div><br>
+        <input class="btn btn-lg btn-primary btn-block" type="submit" value="Rechercher">
+        </form><br>
+        <?php
+            if (isset($_POST["valeur"])&&isset($_POST["method"])) {
+                $valeur=$_POST["valeur"];
+                $con=mysqli_connect("localhost","root","");
+                mysqli_select_db($con,'aumk');
+                if ($_POST["method"] == "ID de la catégorie") {
+                    $reponse=mysqli_query($con,"select * from categore1 where ID_CATEGORE1 = $valeur");?>
+                    <table class="table table-bordered table-hover">
+                        <thead class="table-primary">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nom</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-light">
+                            <tr>
+                    <?php while($donnees = mysqli_fetch_array($reponse)){?>
+                                <th scope="row"><?php echo $donnees[0]; ?></th>
+                                <td><?php echo $donnees[1]; ?></td>
+                    <?php
+                    }?>
+                    </tbody>
+                    </table> 
+                    <?php       
+                }
+                else {
+                    $reponse2=mysqli_query($con,"select * from categore1 where NOM_CATEGORE1 = '$valeur'");?>
+                    <table class="table table-bordered table-hover">
+                        <thead class="table-primary">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nom</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-light">
+                            <tr>
+                    <?php while($donnees = mysqli_fetch_array($reponse2)){?>
+                                <th scope="row"><?php echo $donnees[0]; ?></th>
+                                <td><?php echo $donnees[1]; ?></td>
+                    <?php
+                    }?>
+                    </tbody>
+                    </table> 
+                    <?php
+                }
+            }
+        ?>
+        </main><!-- /.container -->
+        <script type="text/JavaScript">
+        function change_method() {
+        if (document.getElementById("method").value != "Recherche Par :") {
+            var method = document.getElementById("method").value;
+            if (document.getElementById("method").value === "ID de la catégorie") {
+                document.getElementById("valeur").innerHTML="<input id='valeur' class='form-control' type='number' name='valeur' placeholder='"+String(method)+"' required >";   
+            }
+            else{
+                document.getElementById("valeur").innerHTML="<input id='valeur' class='form-control' type='text' name='valeur' placeholder='"+String(method)+"' required >";
+            }
+            }
+        }
+        </script>
+        <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
     </body>
 </html>
