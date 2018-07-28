9ccae0f428d9b7170ea2f99d895959fd071ed7ca
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-29 00:53:20 +0100

    display the results in drop menu

--- search.php
+++ search.php
@@ -28,25 +28,124 @@ session_start();
         $con=mysqli_connect("localhost","root","");
         mysqli_select_db($con,'aumk');
         $reponse = mysqli_query($con,"select * from article where NOM_ARTICLE like '%$search%'");
-        $donnee = mysqli_fetch_array($reponse);
         $reponse2 = mysqli_query($con,"select * from categore1 where NOM_CATEGORE1 like '%$search%'");
-        $donnee2 = mysqli_fetch_array($reponse2);
         $reponse3 = mysqli_query($con,"select * from categore2 where NOM_CATEGORE2 like '%$search%'");
-        $donnee3 = mysqli_fetch_array($reponse3);
     ?>
     <div class="list-form">
     <ul class="list-group">
-  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <li class="list-group-item d-flex justify-content-between align-items-center nav-item dropdown">
     Articles
-    <span class="badge badge-primary badge-pill"><?php echo mysqli_num_rows($reponse);?></span>
+    <span class="badge badge-primary badge-pill nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo mysqli_num_rows($reponse);?></span>
+    <div class="dropdown-menu" aria-labelledby="dropdown01">
+    <table class="table table-hover">
+                <thead class="table-primary">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nom Categorie 2</th>
+                        <th scope="col">Nom Article</th>
+                        <?php
+                        if ($profile[0] == 1||$profile[0] == 3) {?>
+                            <th class="text-center" scope="col" colspan="2" width="1%">Options</th>
+                            <?php
+                        }
+                        ?>
+                    </tr>
+                </thead>
+                <tbody class="table-light">
+            <?php while($donnees = mysqli_fetch_array($reponse)){
+                $donnees1=mysqli_fetch_array(mysqli_query($con,"select NOM_CATEGORE1 from categore1 where ID_CATEGORE1 = $donnees[1]"));
+                ?>
+                    <tr>    
+                        <th scope="row"><?php echo $donnees[0];?></th>
+                        <td><?php echo $donnees1[0]; ?></td>
+                        <td><?php echo $donnees[2]; ?></td>
+                        <?php
+                        if ($profile[0] == 1||$profile[0] == 3) {?>
+                        <td><a href="ajoutement_categorie1.php?id=<?php echo $donnees[0];?>"><img src="res\images\edit-icon.svg" height="30x" title="modifier"></a></td>
+                        <td><a onclick="supprimer(<?php echo $donnees[0]; ?>)" href="#"><img src="res\images\delete-icon.svg" height="30x" title="supprimer"></a></td>
+                        <?php
+                        }
+                        ?>
+                    </tr>
+            <?php
+            }?>
+            </tbody>
+            </table>
+        </div>
   </li>
-  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <li class="list-group-item d-flex justify-content-between align-items-center nav-item dropdown">
     Categorie 1
-    <span class="badge badge-primary badge-pill"><?php echo mysqli_num_rows($reponse2);?></span>
+    <span class="badge badge-primary badge-pill nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo mysqli_num_rows($reponse2);?></span>
+    <div class="dropdown-menu" aria-labelledby="dropdown02">
+    <table class="table table-hover">
+        <thead class="table-primary">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nom</th>
+                <?php
+                if ($profile[0] == 1||$profile[0] == 3) {?>
+                    <th class="text-center" scope="col" colspan="2" width="1%">Options</th>
+                    <?php
+                }
+                ?>
+            </tr>
+        </thead>
+        <tbody class="table-light">
+    <?php while($donnees = mysqli_fetch_array($reponse2)){?>
+            <tr>
+                <th scope="row"><?php echo $donnees[0];?></th>
+                <td><?php echo $donnees[1]; ?></td>
+                <?php
+                if ($profile[0] == 1||$profile[0] == 3) {?>
+                <td><a href="ajoutement_categorie1.php?id=<?php echo $donnees[0];?>"><img src="res\images\edit-icon.svg" height="30x" title="modifier"></a></td>
+                <td><a onclick="supprimer(<?php echo $donnees[0]; ?>)" href="#"><img src="res\images\delete-icon.svg" height="30x" title="supprimer"></a></td>
+                <?php
+                }
+                ?>
+            </tr>
+    <?php
+    }?>
+    </tbody>
+    </table></div>
   </li>
-  <li class="list-group-item d-flex justify-content-between align-items-center">
+  <li class="list-group-item d-flex justify-content-between align-items-center nav-item dropdown">
    Categorie 2
-    <span class="badge badge-primary badge-pill"><?php echo mysqli_num_rows($reponse3);?></span>
+    <span class="badge badge-primary badge-pill nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo mysqli_num_rows($reponse3);?></span>
+    <div class="dropdown-menu" aria-labelledby="dropdown03">
+    <table class="table table-hover">
+        <thead class="table-primary">
+            <tr >
+                <th scope="col">#</th>
+                <th scope="col">Nom Categorie 1</th>
+                <th scope="col">Nom Categorie 2</th>
+                <?php
+                if ($profile[0] == 1||$profile[0] == 3) {?>
+                    <th class="text-center" scope="col" colspan="2" width="1%">Options</th>
+                    <?php
+                }
+                ?>
+            </tr>
+        </thead>
+        <tbody class="table-light">
+    <?php while($donnees = mysqli_fetch_array($reponse3)){
+        $donnees1=mysqli_fetch_array(mysqli_query($con,"select NOM_CATEGORE1 from categore1 where ID_CATEGORE1 = $donnees[1]"));
+        ?>
+            <tr>
+                <th scope="row"><?php echo $donnees[0];?></th>
+                <td><?php echo $donnees1[0]; ?></td>
+                <td><?php echo $donnees[2]; ?></td>
+                <?php
+                if ($profile[0] == 1||$profile[0] == 3) {?>
+                <td><a href="ajoutement_categorie1.php?id=<?php echo $donnees[0];?>"><img src="res\images\edit-icon.svg" height="30x" title="modifier"></a></td>
+                <td><a onclick="supprimer(<?php echo $donnees[0]; ?>)" href="#"><img src="res\images\delete-icon.svg" height="30x" title="supprimer"></a></td>
+                <?php
+                }
+                ?>
+            </tr>
+    <?php
+    }?>
+    </tbody>
+    </table></div>
   </li>
 </ul>
 </div>
