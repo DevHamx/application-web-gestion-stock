26d1391997ac3186f7e80c95e08cb41acc80f43f
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-30 15:41:17 +0100

    change text to an image

--- navbar.php
+++ navbar.php
@@ -77,7 +77,7 @@
       </ul>
       <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="index.php">Deconnecter</a>
+            <a class="nav-link" href="index.php"><img height="30x" src="res/images/sign-out.svg"></a>
           </li>
         </ul>
       <form class="form-inline my-2 my-lg-0" method="GET" action="search.php">
