a33348c35090868aad080dc4863783f51add7da6
Hamza Ghaissi <Hamza.ghaissi@gmail.com>
2018-07-30 15:46:55 +0100

    add the footer to the index page

--- index.php
+++ index.php
@@ -22,8 +22,10 @@ if (isset($_SESSION["id_login"])) {
             <label class="sr-only" for="password">password</label>
             <input class="form-control" type="password" name="password" placeholder="password" required autofocus><br>
             <input class="btn btn-lg btn-primary btn-block" type="submit" value="Log In">
+            <footer>
+                <p class="mt-5 mb-3 text-muted card-footer">&copy; Hamza Ghaissi-AUMK-Juillet 2018</p>
+            </footer>
             <?php 
-                include 'footer.html';
             if(isset($_POST["CIN"])&&isset($_POST['password'])){
                 $var=0;
                 $login1=$_POST['CIN'];
